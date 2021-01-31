8b9c0242cff2fff3c87b42721c54cfbf7ccc9d1e
Shannon TJ <shannontj@shaw.ca>
2021-01-31 13:43:34 -0700

    rest api view of data

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Tweet
 
@@ -11,8 +11,20 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JS or Swift or Java or ios/Android
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data['message'] = 'Not found'
+        status = 404
+
+    return JsonResponse(data, status=status)
