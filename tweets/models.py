af06e8fe5d6a9272447bf4b199324202bce892cd
Shannon TJ <shannontj@shaw.ca>
2021-02-01 19:48:21 -0700

    setup user model

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,14 @@
-from django.db import models
 import random
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
     #id = models.AutoField(primary_key=True)
+    # foreign key, because many users can have many tweets
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
