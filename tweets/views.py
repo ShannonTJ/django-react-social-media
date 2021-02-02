7b863b4ea47739c3a241137f9eee30f78cb22a8a
Shannon TJ <shannontj@shaw.ca>
2021-02-02 14:59:20 -0700

    django rest framework serializer instead of forms

--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from .forms import TweetForm
 from .models import Tweet
+from .serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -18,6 +19,14 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data=request.POST or None)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse({}, status=400)
+
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
     """
     REST API CREATE VIEW (Django Rest Framework)
     """
