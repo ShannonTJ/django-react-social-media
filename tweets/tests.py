31ede87faf59c805667561feae5e114cc9dd1db9
Shannon TJ <shannontj@shaw.ca>
2021-02-02 17:21:24 -0700

    testing like function

--- tweets/tests.py
+++ tweets/tests.py
@@ -14,11 +14,13 @@ class TweetTestCase(TestCase):
         self.user = User.objects.create_user(
             username="abc", password="somepassword")
         Tweet.objects.create(content="my first tweet", user=self.user)
+        Tweet.objects.create(content="my first tweet", user=self.user)
+        Tweet.objects.create(content="my first tweet", user=self.user)
 
     def test_tweet_created(self):
         tweet_obj = Tweet.objects.create(
-            content="my second tweet", user=self.user)
-        self.assertEqual(tweet_obj.id, 2)
+            content="my 4th tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 4)
         self.assertEqual(tweet_obj.user, self.user)
 
     def get_client(self):
@@ -30,5 +32,12 @@ class TweetTestCase(TestCase):
         client = self.get_client()
         response = client.get("/api/tweets/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 1)
-        print(response.json())
+        self.assertEqual(len(response.json()), 3)
+
+    def test_action_like(self):
+        client = self.get_client()
+        response = client.post("/api/tweets/action/",
+                               {"id": 1, "action": "like"})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get("likes")
+        self.assertEqual(like_count, 1)
