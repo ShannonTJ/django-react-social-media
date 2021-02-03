c825c366d46d8a7f84b3dc67c6ffbc5fc71f749e
Shannon TJ <shannontj@shaw.ca>
2021-02-02 17:16:03 -0700

    test tweet list api url

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from rest_framework.test import APIClient
+
 from .models import Tweet
 
 User = get_user_model()
@@ -11,8 +13,22 @@ class TweetTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username="abc", password="somepassword")
+        Tweet.objects.create(content="my first tweet", user=self.user)
 
     def test_tweet_created(self):
-        tweet_obj = Tweet.objects.create(content="my tweet", user=self.user)
-        self.assertEqual(tweet_obj.id, 1)
+        tweet_obj = Tweet.objects.create(
+            content="my second tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 2)
         self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password="somepassword")
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        print(response.json())
