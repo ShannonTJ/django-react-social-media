64aaf78a9119017f793442545f1fadcfd98c2953
Shannon TJ <shannontj@shaw.ca>
2021-02-05 19:48:48 -0700

    display profile edit page and form

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 
+from .forms import ProfileForm
 from .models import Profile
 
-# Create your views here.
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/login?next=/profile/update")
+    user = request.user
+    my_profile = request.user.profile
+    form = ProfileForm(request.POST or None, instance=my_profile)
+
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get("first_name")
+        last_name = form.cleaned_data.get("last_name")
+        email_address = form.cleaned_data.get("email_address")
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email_address = email_address
+        user.save()
+        profile_obj.save()
+    context = {
+        "form": form,
+        "btn_label": "Save",
+        "title": "Update Profile"
+    }
+    return render(request, "profiles/form.html", context)
 
 
 def profile_detail_view(request, username, *args, **kwargs):
@@ -16,4 +40,4 @@ def profile_detail_view(request, username, *args, **kwargs):
         "username": username,
         "profile": profile_obj
     }
-    return render(request, "profiles/detail.html", context)
+    return render(request, "profiles/form.html", context)
