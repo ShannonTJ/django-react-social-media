1716539e200853d72172964b78362404493c16f4
Shannon TJ <shannontj@shaw.ca>
2021-02-05 18:56:10 -0700

    user profile pages

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import profile_detail_view
 
 urlpatterns = [
-    path('profile/<str:username>', profile_detail_view),
+    path('<str:username>', profile_detail_view),
 ]
