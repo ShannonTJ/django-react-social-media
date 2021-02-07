8055355567050df2e4a35050e7bd10c7e7a91c48
Shannon TJ <shannontj@shaw.ca>
2021-02-06 18:28:31 -0700

    pass request to serializers

--- profiles/views.py
+++ profiles/views.py
@@ -45,7 +45,6 @@ def profile_detail_view(request, username, *args, **kwargs):
     if request.user.is_authenticated:
         user = request.user
         is_following = user in profile_obj.followers.all()
-        # is_following = profile_obj in user.following.all()
     context = {
         "username": username,
         "profile": profile_obj,
